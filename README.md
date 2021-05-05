144bbb2f1650fb246bfcbdd94e702c2d40a102fa
Max16 <max16@naver.com>
2021-05-05 15:10:01 +0900

    useTitle

---
+++ README.md
@@ -0,0 +1,18 @@
+## Practical React Hooks
+
+### useState
+
+- [ ] useInput
+- [x] useTabs
+- [x] useTitle
+- [ ] useClick
+- [ ] usePageLeave
+- [ ] useFadeIn
+- [ ] useFullscreen
+- [ ] useHover
+- [ ] useNetwork
+- [ ] useNotification
+- [ ] useScroll
+- [ ] usePreventLeave
+- [ ] useConrifm
+- [ ] useAxios
---
+++ src/useTitle.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+const App = () => {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
+  return <div className="App"></div>;
+};
+
+export default App;
